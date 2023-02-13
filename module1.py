64cb1287b01a05722d3681cc9893809b18fd27a7
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2023-02-13 11:46:56 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,1 +1,13 @@
-
+#Harjutus 6.
+def is_prime(число)->bool:
+    """
+    Määrme is_prime...
+    :parem int arv: Järjend is_prime numbridest
+    :rtype: str
+    """
+    flag=True
+    for i in range(2, число):
+        if число%i==0:
+            flag=False
+            break
+    return flag
